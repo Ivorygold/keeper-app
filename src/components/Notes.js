5548c54433d5e7a5edffd88f82131260f24df226
udemba godswill <godswill.udemna.pg88009@unn.edu.ng>
2023-01-19 16:04:45 +0100

    first commit

---
+++ src/components/Notes.js
@@ -0,0 +1,38 @@
+const Notes = [
+  {
+    key: 1,
+    title: "BETTER",
+    content:
+      "G he goes out as if he a child but will kill her self over heartbreak",
+  },
+
+  {
+    key: 2,
+    title: "savior",
+    content:
+      "G he goes out as if he a child but will kill her self over heartbreak",
+  },
+
+  {
+    key: 3,
+    title: "tiger",
+    content:
+      "G he goes out as if he a child but will kill her self over heartbreak",
+  },
+
+  {
+    key: 4,
+    title: "GOOD",
+    content:
+      "G he goes out as if he a child but will kill her self over heartbreak",
+  },
+
+  {
+    key: 5,
+    title: "BAD",
+    content:
+      "G he goes out as if he a child but will kill her self over heartbreak",
+  },
+];
+
+export default Notes;
